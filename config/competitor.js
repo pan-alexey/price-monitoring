7f1f5384c8bfdcefa58745987bfdf53d94e4c512
Vladislav Fomenko <fvladislav94@gmail.com>
2019-06-10 16:48:31 +0300

    add tdarsenal rules

--- config/competitor.js
+++ config/competitor.js
@@ -1,10 +1,12 @@
 module.exports = {
     "220 вольт" : "220-volt.ru",
     "все инструменты" : "vseinstrumenti.ru",
-    "беру" : "beru.ru",
     "кувалда" : "kuvalda.ru",
+    "арсенал трейдинг" : "tdarsenal.ru",
     "леруа мерлен" : "leroymerlin.ru",
     "бауцентр" : "baucenter.ru",
+    "беру" : "beru.ru",
     "технопоинт" : "technopoint.ru",
+    "онлайн трейд" : "onlinetrade.ru",
 
 }
